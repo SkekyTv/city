4e70d66738bd2600ae59057e2e5b1947c84c4cfc
SkekyTv <skeky.stream@gmail.com>
2023-09-07 07:50:29 +0200

    chore: Change default host

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 const config = {
-  host: process.env.HOST || 'localhost',
+  host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT || 3010,
   gmap: {
     url: process.env.GMAP_API_ENDPOINT,
